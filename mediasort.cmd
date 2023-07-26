edf7ec03fe7b990a7366ff52593c4675aa2e37a9
KhaliDino <103606561+A-KhaliDino@users.noreply.github.com>
2023-07-26 19:48:30 -0400

    Add files via upload

---
+++ mediasort.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'mediasort.exe' with Python DLL being found.
+set PATH=c:\PYTHON~2;%PATH%
+set PYTHONHOME=C:\Python311
+"%~dp0.\mediasort.exe" %*
